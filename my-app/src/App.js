79b9b01f17ce901611d18a0d2e234bb9d07adb03
Persian masters voice <120068514+persianmastersvoice@users.noreply.github.com>
2023-05-08 02:37:45 -0400

    May 8th 2:40 AM
    got the music working!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [songs, setSongs] = useState([]);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/persianmastersvoice/data/e1c6fd8a66c0e99a6270b50d91776c634549b21b/songs/songs.json')
@@ -9,6 +10,53 @@ function App() {
       .then(data => setSongs(data.songs))
       .catch(error => console.error(error));
   }, []);
+
+  const audioFiles = [
+    {
+      title: 'Audio 1',
+      file: 'https://raw.githubusercontent.com/persianmastersvoice/data/main/audio/audio-1.mp3'
+    },
+    {
+      title: 'Audio 2',
+      file: 'https://raw.githubusercontent.com/persianmastersvoice/data/main/audio/audio-2.mp3'
+    },
+    {
+      title: 'Audio 3',
+      file: 'https://raw.githubusercontent.com/persianmastersvoice/data/main/audio/audio-3.mp3'
+    },
+    {
+      title: 'Audio 4',
+      file: 'https://raw.githubusercontent.com/persianmastersvoice/data/main/audio/audio-4.mp3'
+    },
+    {
+      title: 'Audio 5',
+      file: 'https://raw.githubusercontent.com/persianmastersvoice/data/main/audio/audio-5.mp3'
+    }
+  ];
+
+  const handlePlay = () => {
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.play();
+  };
+
+  const handlePause = () => {
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.pause();
+  };
+
+  const handleStop = () => {
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+  };
+
+  const handlePrevious = () => {
+    setCurrentSongIndex(currentSongIndex === 0 ? audioFiles.length - 1 : currentSongIndex - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentSongIndex(currentSongIndex === audioFiles.length - 1 ? 0 : currentSongIndex + 1);
+  };
 
   return (
     <div>
@@ -33,6 +81,16 @@ function App() {
           ))}
         </tbody>
       </table>
+      <h2>Audio Player</h2>
+      <audio id="audio-player" src={audioFiles[currentSongIndex].file} controls></audio>
+      <p>
+        <strong>Now playing: </strong>{audioFiles[currentSongIndex].title}
+      </p>
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 }
